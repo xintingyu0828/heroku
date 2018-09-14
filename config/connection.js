9f0a4ac001d40642017937a20ab92eedafb64fe8
Xinting Yu <xintingyu@dhcp-10-101-118-254.wireless.northwestern.private>
2018-09-14 12:27:48 -0500

    refresh

--- config/connection.js
+++ config/connection.js
@@ -24,10 +24,10 @@ if (process.env.JAWSDB_URL){
 connection=mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection=mysql.createConnection({
-    host:'localhost',
-    user:'ue6ecy2utvzmb21',
-    passward:'zhuavdvvsgi15xr6',
-    database:'d2sr48blwfwctkjy'
+    host:'ls0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'qngbvvh2rdt99qa4',
+    passward:'mjyftmv2y43cwse1',
+    database:'ac1qn1faoa2rhfhq'
 
   });
 };
